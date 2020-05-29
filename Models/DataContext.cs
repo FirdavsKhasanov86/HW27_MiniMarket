7e150bfed473e60c80734346776bf251b9e5d00a
Singl <Singl@DESKTOP-TSIH1IQ>
2020-05-29 20:05:17 +0500

    Finish MiniMarket

---
+++ Models/DataContext.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace HW27_MiniMarket.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductCategory> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            model.Entity<ProductCategory>().HasData(
+            new ProductCategory()
+            {
+                Id = 1,
+                Category = "SSD"
+            },
+            new ProductCategory()
+            {
+                Id = 2,
+                Category = "CPU"
+            },
+            new ProductCategory()
+            {
+                Id = 3,
+                Category = "VGA"
+            });
+        }
+    }
+}
